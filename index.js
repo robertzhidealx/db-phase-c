9c0d9d15a21a1e934e7fd644e1a9daaef574b94a
Jessie Luo <jluo30@jhu.edu>
2022-04-07 21:45:45 -0400

    add repository

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-const fs = require("fs");
-require("dotenv").config();
+const fs = require('fs');
+require('dotenv').config();
 
 const token = process.env.TOKEN;
 console.log(token);
 
 // Organization(orgID, login, name, description, email, location, type, createdAt, updatedAt)
 async function organization() {
-  const orgs = ["facebook", "google", "amzn"];
-  const db = [];
-  for (const org of orgs) {
-    const {
-      id,
-      login,
-      name,
-      description,
-      email,
-      created_at,
-      updated_at,
-      location,
-      type,
-    } = await get(`https://api.github.com/orgs/${org}`);
-    db.push(
-      `${id},${login},${name},${description},${email},${created_at},${updated_at},${location},${type}`
-    );
-  }
-  writeFile("organization.txt", db.join("\n"));
+	const orgs = [ 'facebook', 'google', 'amzn', 'angular' ];
+	const db = [];
+	for (const org of orgs) {
+		const { id, login, name, description, email, created_at, updated_at, location, type } = await get(
+			`https://api.github.com/orgs/${org}`
+		);
+		db.push(`${id},${login},${name},${description},${email},${created_at},${updated_at},${location},${type}`);
+	}
+	writeFile('organization.txt', db.join('\n'));
 }
 
 // Repository(repoID, name, description, url, forksCount, stargazersCount, watchersCount, openIssuesCount)
-async function repository() {}
+async function repository() {
+	const pairs = [
+		[ 'octocat', 'hello-world' ],
+		[ 'Anduin2017', 'HowToCook' ],
+		[ 'sveltejs', 'svelte' ],
+		[ 'facebook', 'react' ],
+		[ 'angular', 'angular' ],
+		[ 'vuejs', 'vue' ],
+		[ 'vercel', 'next.js' ],
+		[ 'nuxt', 'nuxt.js' ],
+		[ 'microsoft', 'Web-Dev-For-Beginners' ],
+		[ 'airbnb', 'javascript' ],
+		[ 'sveltejs', 'kit' ]
+	];
+	const db = [];
+	for (const pair of pairs) {
+		owner = pair[0];
+		repo = pair[1];
+		const {
+			id,
+			name,
+			description,
+			url,
+			forks_count,
+			stargazers_count,
+			watchers_count,
+			open_issues_count
+		} = await get(`https://api.github.com/repos/${owner}/${repo}`);
+		db.push(
+			`${id},${name},${description},${url},${forks_count},${stargazers_count},${watchers_count},${open_issues_count}`
+		);
+	}
+	writeFile('repository.txt', db.join('\n'));
+}
 
 //User(userID, login, url, type)
 async function user() {}
@@ -52,22 +75,22 @@ async function downloads() {}
 async function downloadsOnDate() {}
 
 function get(url) {
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  };
-  return fetch(url, options).then((res) => res.json());
+	const options = {
+		method: 'GET',
+		headers: {
+			Authorization: `token ${token}`
+		}
+	};
+	return fetch(url, options).then((res) => res.json());
 }
 
 function writeFile(fileName, content) {
-  fs.writeFile(fileName, content, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
+	fs.writeFile(fileName, content, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+	});
 }
 
-organization();
+repository();

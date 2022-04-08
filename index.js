58e0ad43a2acbb9c97bc5d95fc721619a5443e3f
Robert Zhang <jiaxuanzhang066@gmail.com>
2022-04-07 20:03:15 -0400

    feat: create basic structure

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-fetch("https://api.github.com/orgs/facebook")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const fs = require("fs");
+
+async function organization() {
+  const orgs = ["facebook", "google", "amzn"];
+  for (const org of orgs) {
+    const data = await get(`https://api.github.com/orgs/${org}`);
+    console.log(data);
+  }
+  writeFile("organization.txt", JSON.stringify(orgs));
+}
+
+function get(url) {
+  const options = {
+    method: "GET",
+  };
+  return fetch(url, options).then((res) => res.json());
+}
+
+function writeFile(fileName, content) {
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+organization();
